21ed285d555d7184c6d75d2cd1bbf28b9a3ab09d
David Zhang <dyzhang32@gmail.com>
2021-09-16 16:13:36 +0100

    tidy and add pdf for resume

--- render_cv.r
+++ render_cv.r
@@ -24,3 +24,14 @@ pagedown::chrome_print(input = tmp_html_cv_loc,
 rmarkdown::render("resume.rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "resume.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("resume.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "dz_resume.pdf")
+
