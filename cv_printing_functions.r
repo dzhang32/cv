23a0f1fe9b4ee937cbc8710d024ed863322135f8
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-09-13 18:32:14 +0100

    refactor: format using air

--- cv_printing_functions.r
+++ cv_printing_functions.r
@@ -13,19 +13,23 @@
 #' @param sheet_is_publicly_readable If you're using google sheets for data,
 #'   is the sheet publicly available? (Makes authorization easier.)
 #' @return A new `CV_Printer` object.
-create_CV_object <-  function(data_location,
-                              pdf_mode = FALSE,
-                              sheet_is_publicly_readable = TRUE) {
-  
+create_CV_object <- function(
+  data_location,
+  pdf_mode = FALSE,
+  sheet_is_publicly_readable = TRUE
+) {
   cv <- list(
     pdf_mode = pdf_mode,
     links = c()
   )
-  
-  is_google_sheets_location <- stringr::str_detect(data_location, "docs\\.google\\.com")
-  
-  if(is_google_sheets_location){
-    if(sheet_is_publicly_readable){
+
+  is_google_sheets_location <- stringr::str_detect(
+    data_location,
+    "docs\\.google\\.com"
+  )
+
+  if (is_google_sheets_location) {
+    if (sheet_is_publicly_readable) {
       # This tells google sheets to not try and authenticate. Note that this will only
       # work if your sheet has sharing set to "anyone with link can view"
       googlesheets4::gs4_deauth()
@@ -35,39 +39,58 @@ create_CV_object <-  function(data_location,
       # designate project-specific cache so we can render Rmd without problems
       options(gargle_oauth_cache = ".secrets")
     }
-    
-    read_gsheet <- function(sheet_id){
-      googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
+
+    read_gsheet <- function(sheet_id) {
+      googlesheets4::read_sheet(
+        data_location,
+        sheet = sheet_id,
+        skip = 1,
+        col_types = "c"
+      )
     }
-    cv$entries_data  <- read_gsheet(sheet_id = "entries")
-    cv$skills        <- read_gsheet(sheet_id = "language_skills")
-    cv$text_blocks   <- read_gsheet(sheet_id = "text_blocks")
-    cv$contact_info  <- read_gsheet(sheet_id = "contact_info")
+    cv$entries_data <- read_gsheet(sheet_id = "entries")
+    cv$skills <- read_gsheet(sheet_id = "language_skills")
+    cv$text_blocks <- read_gsheet(sheet_id = "text_blocks")
+    cv$contact_info <- read_gsheet(sheet_id = "contact_info")
   } else {
     # Want to go old-school with csvs?
-    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1)
-    cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"), skip = 1)
-    cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
-    cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
+    cv$entries_data <- readr::read_csv(
+      paste0(data_location, "entries.csv"),
+      skip = 1
+    )
+    cv$skills <- readr::read_csv(
+      paste0(data_location, "language_skills.csv"),
+      skip = 1
+    )
+    cv$text_blocks <- readr::read_csv(
+      paste0(data_location, "text_blocks.csv"),
+      skip = 1
+    )
+    cv$contact_info <- readr::read_csv(
+      paste0(data_location, "contact_info.csv"),
+      skip = 1
+    )
   }
-  
-  
-  extract_year <- function(dates){
+
+  extract_year <- function(dates) {
     date_year <- stringr::str_extract(dates, "(20|19)[0-9]{2}")
-    date_year[is.na(date_year)] <- lubridate::year(lubridate::ymd(Sys.Date())) + 10
-    
+    date_year[is.na(date_year)] <- lubridate::year(lubridate::ymd(Sys.Date())) +
+      10
+
     date_year
   }
-  
-  parse_dates <- function(dates){
-    
-    date_month <- stringr::str_extract(dates, "(\\w+|\\d+)(?=(\\s|\\/|-)(20|19)[0-9]{2})")
+
+  parse_dates <- function(dates) {
+    date_month <- stringr::str_extract(
+      dates,
+      "(\\w+|\\d+)(?=(\\s|\\/|-)(20|19)[0-9]{2})"
+    )
     date_month[is.na(date_month)] <- "1"
-    
+
     paste("1", date_month, extract_year(dates), sep = "-") %>%
       lubridate::dmy()
   }
-  
+
   # Clean up entries dataframe to format we need it for printing
   cv$entries_data %<>%
     tidyr::unite(
@@ -77,7 +100,11 @@ create_CV_object <-  function(data_location,
       na.rm = TRUE
     ) %>%
     dplyr::mutate(
-      description_bullets = ifelse(description_bullets != "", paste0("- ", description_bullets), ""),
+      description_bullets = ifelse(
+        description_bullets != "",
+        paste0("- ", description_bullets),
+        ""
+      ),
       start = ifelse(start == "NULL", NA, start),
       end = ifelse(end == "NULL", NA, end),
       start_year = extract_year(start),
@@ -87,54 +114,55 @@ create_CV_object <-  function(data_location,
       no_end = is.na(end),
       has_end = !no_end,
       timeline = dplyr::case_when(
-        no_start  & no_end  ~ "N/A",
-        no_start  & has_end ~ as.character(end),
-        has_start & no_end  ~ paste("Current", "-", start),
-        TRUE                ~ paste(end, "-", start)
+        no_start & no_end ~ "N/A",
+        no_start & has_end ~ as.character(end),
+        has_start & no_end ~ paste("Current", "-", start),
+        TRUE ~ paste(end, "-", start)
       )
     ) %>%
     dplyr::arrange(desc(parse_dates(end))) %>%
     dplyr::mutate_all(~ ifelse(is.na(.), 'N/A', .))
-  
+
   cv
 }
 
 
 # Remove links from a text block and add to internal list
-sanitize_links <- function(cv, text){
-  if(cv$pdf_mode){
+sanitize_links <- function(cv, text) {
+  if (cv$pdf_mode) {
     link_titles <- stringr::str_extract_all(text, '(?<=\\[).+?(?=\\])')[[1]]
-    link_destinations <- stringr::str_extract_all(text, '(?<=\\().+?(?=\\))')[[1]]
-    
+    link_destinations <- stringr::str_extract_all(text, '(?<=\\().+?(?=\\))')[[
+      1
+    ]]
+
     n_links <- length(cv$links)
     n_new_links <- length(link_titles)
-    
-    if(n_new_links > 0){
+
+    if (n_new_links > 0) {
       # add links to links array
       cv$links <- c(cv$links, link_destinations)
-      
+
       # Build map of link destination to superscript
       link_superscript_mappings <- purrr::set_names(
         paste0("<sup>", (1:n_new_links) + n_links, "</sup>"),
         paste0("(", link_destinations, ")")
       )
-      
+
       # Replace the link destination and remove square brackets for title
       text <- text %>%
         stringr::str_replace_all(stringr::fixed(link_superscript_mappings)) %>%
         stringr::str_replace_all('\\[(.+?)\\]', "\\1")
     }
   }
-  
+
   list(cv = cv, text = text)
 }
 
 
 #' @description Take a position data frame and the section id desired and prints the section to markdown.
 #' @param section_id ID of the entries section to be printed as encoded by the `section` column of the `entries` table
-print_section <- function(cv, section_id, glue_template = "default"){
-  
-  if(glue_template == "default"){
+print_section <- function(cv, section_id, glue_template = "default") {
+  if (glue_template == "default") {
     glue_template <- "
 ### {title}
 
@@ -147,85 +175,84 @@ print_section <- function(cv, section_id, glue_template = "default"){
 {description_bullets}
 \n\n"
   }
-  
+
   section_data <- dplyr::filter(cv$entries_data, section == section_id)
-  
+
   # Take entire entries data frame and removes the links in descending order
   # so links for the same position are right next to each other in number.
-  for(i in 1:nrow(section_data)){
-    for(col in c('title', 'description_bullets')){
+  for (i in 1:nrow(section_data)) {
+    for (col in c('title', 'description_bullets')) {
       strip_res <- sanitize_links(cv, section_data[i, col])
       section_data[i, col] <- strip_res$text
       cv <- strip_res$cv
     }
   }
-  
+
   print(glue::glue_data(section_data, glue_template))
-  
+
   invisible(strip_res$cv)
 }
-
 
 
 #' @description Prints out text block identified by a given label.
 #' @param label ID of the text block to print as encoded in `label` column of `text_blocks` table.
-print_text_block <- function(cv, label){
+print_text_block <- function(cv, label) {
   text_block <- dplyr::filter(cv$text_blocks, loc == label) %>%
     dplyr::pull(text)
-  
+
   strip_res <- sanitize_links(cv, text_block)
-  
+
   cat(strip_res$text)
-  
+
   invisible(strip_res$cv)
 }
-
 
 
 #' @description Construct a bar chart of skills
 #' @param out_of The relative maximum for skills. Used to set what a fully filled in skill bar is.
 print_skills <- function(cv, type_of_interest) {
-  cv$skills %>% 
-    dplyr::filter(type == type_of_interest) %>% 
+  cv$skills %>%
+    dplyr::filter(type == type_of_interest) %>%
     glue::glue_data(
       "- <i class='{icon_type} fa-{icon} skills'></i> {skill}"
-    ) %>% 
+    ) %>%
     print()
-  
+
   invisible(cv)
 }
-
 
 
 #' @description List of all links in document labeled by their superscript integer.
 print_links <- function(cv) {
   n_links <- length(cv$links)
   if (n_links > 0) {
-    cat("
+    cat(
+      "
 Links {data-icon=link}
 --------------------------------------------------------------------------------
 
 <br>
 
 
-")
-    
+"
+    )
+
     purrr::walk2(cv$links, 1:n_links, function(link, index) {
       print(glue::glue('{index}. {link}'))
     })
   }
-  
+
   invisible(cv)
 }
 
 
-
 #' @description Contact information section with icons
-print_contact_info <- function(cv){
+print_contact_info <- function(cv) {
   glue::glue_data(
     cv$contact_info,
     "- <i class='fa fa-{icon}'></i> {contact}"
-  ) %>% print()
-  
+  ) %>%
+    print()
+
   invisible(cv)
 }
--- render_cv.r
+++ render_cv.r
@@ -1,15 +1,17 @@
 # Knit CV to HTML.
-rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = FALSE),
-                  output_file = "docs/index.html")
+rmarkdown::render(
+    "cv.rmd",
+    params = list(pdf_mode = FALSE),
+    output_file = "docs/index.html"
+)
 
 # Knit CV to temporary HTML.
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("cv.rmd",
-                  params = list(pdf_mode = FALSE),
-                  output_file = tmp_html_cv_loc)
+rmarkdown::render(
+    "cv.rmd",
+    params = list(pdf_mode = FALSE),
+    output_file = tmp_html_cv_loc
+)
 
 # Convert temporary HTML to PDF.
-pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "docs/dz_cv.pdf")
-
+pagedown::chrome_print(input = tmp_html_cv_loc, output = "docs/dz_cv.pdf")
