e525281fabcf37fa379149f62035d5678990d36f
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-09-13 18:21:48 +0100

    docs: simplify docs

--- render_cv.r
+++ render_cv.r
@@ -1,22 +1,15 @@
-# This script builds both the HTML and PDF versions of your CV
-
-# If you wanted to speed up rendering for googlesheets driven CVs you could use
-# this script to cache a version of the CV_Printer class with data already
-# loaded and load the cached version in the .Rmd instead of re-fetching it twice
-# for the HTML and PDF rendering. This exercise is left to the reader.
-
-# Knit cv to HTML version
+# Knit CV to HTML.
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "docs/index.html")
 
-# Knit the PDF version to temporary html location
+# Knit CV to temporary HTML.
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = tmp_html_cv_loc)
 
-# Convert to PDF using Pagedown
+# Convert temporary HTML to PDF.
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "docs/dz_cv.pdf")
 
